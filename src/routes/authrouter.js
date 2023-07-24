f3020c9c0496656a7bffdc1211942e07f06358cf
shaik11r <nadeencool11@gmail.com>
2023-07-24 12:38:03 +0530

    added middlewares for auth

--- src/routes/authrouter.js
+++ src/routes/authrouter.js
@@ -1,11 +1,12 @@
 const authController=require('../services/authService');
-
+const authMiddleware=require('../middleware/authMiddleware');
 const routes=(app)=>{
-    app.post('/api/v1/auth/signup',
+    app.post('/api/v1/auth/signup',authMiddleware.validateSignupRequest,
     authController.signup);
 
-    app.post('/api/v1/auth/sigin',authController.signin);
+    app.post('/api/v1/auth/sigin',authMiddleware.validateSigninRequest,authController.signin);
 
-    app.patch('/api/v1/auth/reset',authController.resetPassword);
+    app.patch('/api/v1/auth/reset',authMiddleware.isAuthenticated,authController.resetPassword);
+    
 };
 module.exports=routes;
