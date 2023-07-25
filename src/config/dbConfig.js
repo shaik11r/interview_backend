4429d56f620d91e1c4bd8d54827668d90490aa89
shaik11r <nadeencool11@gmail.com>
2023-07-25 14:39:22 +0530

    added create,update,get,getall controllers

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,9 +1,5 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-console.log(process.env.PASSWORD);
-const username = "codeonlybro";
-const password = "YIjtEM3h2xI01I6H"; // Replace this with your actual password
-const clusterName = "cluster0.amrmjli.mongodb.net/interview_backendd";
 
 const uri = `mongodb+srv://${encodeURIComponent(process.env.DBUSERNAME)}:${encodeURIComponent(process.env.PASSWORD)}@${
   process.env.CLUSTERNAME
