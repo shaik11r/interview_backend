a3f8b0e350054a1b769aca19f78b78a016d11dd1
shaik11r <nadeencool11@gmail.com>
2023-07-25 13:43:25 +0530

    modified config and secured sensitive data

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
-
+require("dotenv").config();
+console.log(process.env.PASSWORD);
 const username = "codeonlybro";
 const password = "YIjtEM3h2xI01I6H"; // Replace this with your actual password
 const clusterName = "cluster0.amrmjli.mongodb.net/interview_backendd";
 
-const uri = `mongodb+srv://${encodeURIComponent(username)}:${encodeURIComponent(
-  password
-)}@${clusterName}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${encodeURIComponent(process.env.DBUSERNAME)}:${encodeURIComponent(process.env.PASSWORD)}@${
+  process.env.CLUSTERNAME
+}?retryWrites=true&w=majority`;
 
 async function connectToDb() {
   try {
